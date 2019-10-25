788915a01f7bd8be4f4efef7b019573140821f41
Marac <47771012+MarkoNov@users.noreply.github.com>
2019-10-25 18:31:44 +0200

    Gave up on selection for now

--- test-folders.com/js/scripts.js
+++ test-folders.com/js/scripts.js
@@ -56,10 +56,18 @@ function getSelectedText() {
       $("#items > li").click(function(){
           switch(this.id){
               case "copy":
-                  //var copyText = $(event.target);
-                  var copyText = CurrentSelection.Selector.getSelected();
-                  console.log(copyText.htmlText);
+                  console.log("In development");
                  // document.execCommand("copy");
+                  console.log("You have selected "+$(this).text());
+                  break;
+              case "paste":
+                  console.log("In development");
+
+                  console.log("You have selected "+$(this).text());
+                  break;
+              case "delete":
+                  console.log("In development");
+
                   console.log("You have selected "+$(this).text());
                   break;
           }
@@ -116,80 +124,6 @@ $(".folder").dblclick(function () {
       });
 
 
-
-
-
-
-
-
-      var parentContainerId = "yeet"
-		
-	if(!window.CurrentSelection){
-		CurrentSelection = {}
-	}
-	
-	CurrentSelection.Selector = {}
-	
-	//get the current selection
-	CurrentSelection.Selector.getSelected = function(){
-		var sel = '';
-		if(window.getSelection){
-			sel = window.getSelection()
-		}
-		else if(document.getSelection){
-			sel = document.getSelection()
-		}
-		else if(document.selection){
-			sel = document.selection.createRange()
-        }
-        window.alert(sel); //ne dela bez ostaloga V
-		return sel
-	}
-	//function to be called on mouseup
-	CurrentSelection.Selector.mouseup = function(){
-		
-		var st = CurrentSelection.Selector.getSelected()
-		if(document.selection && !window.getSelection){
-			var range = st
-			range.pasteHTML("<span class='selectedText'>" + range.htmlText + "</span>");			
-		}
-		else{
-			var range = st.getRangeAt(0)    
-			var newNode = document.createElement("span");
-			newNode.setAttribute("class", "selectedText");
-			range.surroundContents(newNode);
-			//
-		   var getTitle = newNode.innerHTML;
-		   newNode.setAttribute("title", getTitle);
-           
-		   //
-		   var popDiv = document.createElement('span');
-		   popDiv.setAttribute('class', 'popDiv');
-		   popDiv.innerHTML = getTitle;
-
-		   if(newNode.innerHTML.length > 0) {
-		    newNode.appendChild(popDiv);
-		   }		   
-		   //Remove Selection: To avoid extra text selection in IE  
-		   if (window.getSelection) {
-		     window.getSelection().removeAllRanges();
-		   }
-	       else if (document.selection){ 
-	        document.selection.empty();
-	       }
-	       //
-		}
-	}
-        
-	$(function(){
-
-		$("#"+parentContainerId).on('mouseup', function(){
-			$('span.selectedText').contents().unwrap();
-			$(this).find('span.popDiv').remove();			
-		});
-
-		$("#"+parentContainerId).bind("mouseup",CurrentSelection.Selector.mouseup);	
-	})
 };
 
  
